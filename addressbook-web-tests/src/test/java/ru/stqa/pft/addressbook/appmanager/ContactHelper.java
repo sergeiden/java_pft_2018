5760f10885d0afe8df1c77f1b57e885b5b7d587a
sergeiden <sd100578@yandex.ru>
2018-08-12 23:28:06 +0300

    Добавлено кэширование
    Добавлено кэширование списка контактов и групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.List;
 
@@ -67,33 +68,42 @@ public class ContactHelper extends HelperBase {
     initContactCreation();
     fillContactForm(contact, true);
     submitContact();
+    contactCache = null;
   }
 
   public void delete(int id) {
     selectContactById(id);
     deleteContact();
     confirmContactDeletion();
+    contactCache = null;
   }
 
   public void modify(ContactData contact) {
     editContact(contact.getId());
     fillContactForm(contact, false);
     submitContactModification();
+    contactCache = null;
   }
 
   public boolean isThereContact() {
     return isElementPresent(By.name("selected[]"));
   }
 
+  private Contacts contactCache = null;
+
+
   public Contacts all() {
-    Contacts contacts = new Contacts();
+    if (contactCache != null){
+      return new Contacts(contactCache);
+    }
+    Contacts contactCache = new Contacts();
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for (WebElement element : elements) {
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
       String name = element.findElement(By.xpath(".//td[3]")).getText();
       String lastname = element.findElement(By.xpath(".//td[2]")).getText();
-      contacts.add(new ContactData().withId(id).withName(name).withLastname(lastname));
+      contactCache.add(new ContactData().withId(id).withName(name).withLastname(lastname));
     }
-    return contacts;
+    return contactCache;
   }
 }
