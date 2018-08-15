ca864f7517b1ffa5764f9a5c22e6da15cdb444ed
sergeiden <sd100578@yandex.ru>
2018-08-15 21:04:31 +0300

    DataProvider->чтение данных из файла
    В DataProvider реализовано чтение данных из файла.
    Тест GroupCreation переделан для принятия объекта с тетовыми данными.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,10 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,17 +22,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{"test 1", "header 1", "footer 1" });
-    list.add(new Object[]{"test 2", "header 2", "footer 2" });
-    list.add(new Object[]{"test 3", "header 3", "footer 3" });
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
   @Test(invocationCount = 1, dataProvider = "validGroups")
-  public void testGroupCreation(String name, String header, String footer) {
-    GroupData group = new GroupData().withName(name).withFooter(footer).withHeader(header);
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
