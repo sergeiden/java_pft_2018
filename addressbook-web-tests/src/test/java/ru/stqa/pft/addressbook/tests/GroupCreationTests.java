8806de26ccaee476f07e522e82313c07b4aac0dd
sergeiden <sd100578@yandex.ru>
2018-08-16 22:44:07 +0300

    Добавлено протоколирование
    Добавлено протоколирование с использованием библиотеки logback-classic.
    Настроен файл свойств протоколирования logback.xml и запись протокола в
    файл.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -23,6 +25,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by serg on 20.07.2018.
  */
 public class GroupCreationTests extends TestBase {
+
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
